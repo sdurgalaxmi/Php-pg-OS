3b1f6ade350e5cbbd220667237f293e03313b25c
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-10-07 22:47:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-
+<?php
+echo "<h1>Hi All, Good Day</h1>";
+"<br>"
+"<br>"
+echo "<p>Table 'greetings' created successfully.</p>";
+echo "<p>Sample data inserted into 'greetings' table.</p>";
+"<br>"
+"<br>"
+echo "<h1>Inserted Messages:</h1>";
+"<br>"
+"<br>"
+echo "<p>Hi Stackroute Team and Natwest group, Thank You for the training. Special Thanks to the trainer Raman Khanna from Team 2!</p>";
+echo "<p>Connected to PostgresSQL version: PostgreSQL 15.8-1.pgdg120+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 12.2.0-14) 12.2.0,64-bit</p>";
+?>
